fd0de66259a963ba73c3640af4f075a38ce8d606
McLoy <tkachenko.vlad@hotmail.com>
2017-01-20 20:26:39 +0200

    base patterns: Adapter

---
+++ BasePatterns/src/ua/vtkachenko/proselyte/origin/structural/adapter/AdapterJavaToDatabase.java
@@ -0,0 +1,23 @@
+package ua.vtkachenko.proselyte.origin.structural.adapter;
+
+public class AdapterJavaToDatabase  extends JavaApplication implements Database{
+    @Override
+    public void insert() {
+        saveObject();
+    }
+
+    @Override
+    public void update() {
+        updateObject();
+    }
+
+    @Override
+    public void select() {
+        loadObject();
+    }
+
+    @Override
+    public void remove() {
+        deleteObject();
+    }
+}
---
+++ BasePatterns/src/ua/vtkachenko/proselyte/origin/structural/adapter/JavaApplication.java
@@ -0,0 +1,20 @@
+package ua.vtkachenko.proselyte.origin.structural.adapter;
+
+public class JavaApplication {
+
+    public void saveObject(){
+        System.out.println("Save java object");
+    }
+
+    public void updateObject(){
+        System.out.println("Updating java object");
+    }
+
+    public void loadObject(){
+        System.out.println("Loading java object");
+    }
+
+    public void deleteObject(){
+        System.out.println("Deleting java object");
+    }
+}
