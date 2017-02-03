8b9c47ec43d8bfe5bedf7ef3332657d214481262
McLoy <tkachenko.vlad@hotmail.com>
2017-02-03 22:01:53 +0200

    Spring MVC - example

--- Spring4WebMVCMaven/src/main/webapp/test.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>My title</title>
-</head>
-<body>
-    <b>test</b>
-</body>
-</html>
--- SpringMVC-sampleApp/src/main/java/ua/vtkachenko/controller/InController.java
+++ SpringMVC-sampleApp/src/main/java/ua/vtkachenko/controller/InController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
-    @RequestMapping(value = "/")
-    public String home(){
-        return "test";
+public class InController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "index";
     }
 }
