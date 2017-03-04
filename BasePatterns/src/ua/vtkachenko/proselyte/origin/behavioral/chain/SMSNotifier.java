55775b97bdf36f897eb4b162301d84bd43460f46
McLoy <tkachenko.vlad@hotmail.com>
2017-03-04 13:36:35 +0200

    base patterns: chain

---
+++ BasePatterns/src/ua/vtkachenko/proselyte/origin/behavioral/chain/SMSNotifier.java
@@ -0,0 +1,15 @@
+package ua.vtkachenko.proselyte.origin.behavioral.chain;
+
+public class SMSNotifier extends Notifier {
+
+    public SMSNotifier(int priority) {
+        super(priority);
+    }
+
+    @Override
+    public void write(String message) {
+        System.out.println("Sending sms to manager: " + message);
+    }
+
+
+}
