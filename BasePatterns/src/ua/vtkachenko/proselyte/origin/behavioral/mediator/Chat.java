809e5592a175b1ae893f4a3e181b35927396bafe
McLoy <tkachenko.vlad@hotmail.com>
2017-03-15 00:00:09 +0200

    base patterns^ mediator

---
+++ BasePatterns/src/ua/vtkachenko/proselyte/origin/behavioral/mediator/Chat.java
@@ -0,0 +1,5 @@
+package ua.vtkachenko.proselyte.origin.behavioral.mediator;
+
+public interface Chat {
+    public void sendMessage(String message, User user);
+}
---
+++ BasePatterns/src/ua/vtkachenko/proselyte/origin/behavioral/mediator/User.java
@@ -0,0 +1,7 @@
+package ua.vtkachenko.proselyte.origin.behavioral.mediator;
+
+public interface User {
+    public void sendMessage(String message);
+
+    public void getMessage(String message);
+}
