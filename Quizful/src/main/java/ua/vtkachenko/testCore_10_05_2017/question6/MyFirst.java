70ff8458b62514a87a71eeb0caab3fa3567fdd58
McLoy <tkachenko.vlad@hotmail.com>
2017-05-10 11:18:40 +0300

    core tests

---
+++ Quizful/src/main/java/ua/vtkachenko/testCore_10_05_2017/question6/MyFirst.java
@@ -0,0 +1,11 @@
+package ua.vtkachenko.testCore_10_05_2017.question6;
+
+public class MyFirst {
+    public static void main(String[] args){
+        int i, j;
+        i = 10;
+        j = 20;
+        while(++i < --j);
+        System.out.println(i);
+    }
+}
