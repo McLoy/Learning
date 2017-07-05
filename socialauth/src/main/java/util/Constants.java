5dd24554b335c73499ec2f9b96b95789e5a9fd2b
McLoy <tkachenko.vlad@hotmail.com>
2017-07-05 13:32:57 +0300

    JavaScript

---
+++ socialauth/src/main/java/util/Constants.java
@@ -0,0 +1,12 @@
+package util;
+
+/**
+ * @author Vladyslav T - 17-Jun-17.
+ * @version 1.0
+ */
+public interface Constants {
+    String REQUEST_TYPE = "requestType";
+    String REGISTRATION = "registration";
+    String IMPORT_CONTACTS = "importContacts";
+    String SHARE = "share";
+}
