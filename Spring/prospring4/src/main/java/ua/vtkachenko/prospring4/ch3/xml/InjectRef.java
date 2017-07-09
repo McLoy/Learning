5731e9a9fe59482ad2aecccdc841b1e9936c0472
McLoy <tkachenko.vlad@hotmail.com>
2017-07-09 22:34:00 +0300

    update

---
+++ Spring/prospring4/src/main/java/ua/vtkachenko/prospring4/ch3/xml/InjectRef.java
@@ -0,0 +1,30 @@
+package ua.vtkachenko.prospring4.ch3.xml;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+import ua.vtkachenko.prospring4.ch3.Oracle;
+
+/**
+ * @author Vladyslav T
+ * @version 1.0
+ */
+public class InjectRef {
+    private Oracle oracle;
+
+    public void setOracle(Oracle oracle) {
+        this.oracle = oracle;
+    }
+
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:META-INF/spring/app-context-xml.xml");
+        ctx.refresh();
+
+        InjectRef injectRef = (InjectRef) ctx.getBean("injectRef");
+        System.out.println(injectRef);
+    }
+
+    @Override
+    public String toString() {
+        return oracle.defineMeaningOfLife();
+    }
+}
