46c56dafd965889373e8cad9bd7214472a6f41fc
McLoy <tkachenko.vlad@hotmail.com>
2017-09-25 21:27:13 +0200

    use Spring Environment and PropertySource

---
+++ Spring/prospring4/src/main/java/ua/vtkachenko/prospring4/ch4/Environment/PlaceHolderSample.java
@@ -0,0 +1,15 @@
+package ua.vtkachenko.prospring4.ch4.Environment;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class PlaceHolderSample {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:META-INF/spring/app-context-xml.xml");
+        ctx.refresh();
+
+        AppProperty appProperty = ctx.getBean("appProperty", AppProperty.class);
+        System.out.println("application.home: " + appProperty.getApplicationHome());
+        System.out.println("user.home: " + appProperty.getUserHome());
+    }
+}
